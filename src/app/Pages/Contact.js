caea37fd7532d017179df9fb2c8eafce4b6b1542
hrishi-71 <hrishid71@gmail.com>
2025-08-08 22:51:25 +0530

    Update Contact.js

--- src/app/Pages/Contact.js
+++ src/app/Pages/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
               <h3 className="text-center">Contact With Us</h3>
             </Card.Header>
             <Card.Body>
-              <form name="contact" method="post">
+              <form name="contact" method="post" data-netlify="true">
                 {/* Hidden input for Netlify */}
                 <input type="hidden" name="form-name" value="contact" />
 
